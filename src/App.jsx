7cdbe83afb36960009ecc9948562847673a423a5
Adrian McBroom <adrianmcb2000@gmail.com>
2024-04-16 10:58:26 -0700

    initialized files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {}
+
+
+
+export default App;
